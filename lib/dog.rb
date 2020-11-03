733da5e3608800f55859022c3d2eba8446d688d0
claire <clairebontempo@gmail.com>
2020-11-03 12:51:30 -0800

    commit tree 1098a2fa968abdd9adbdf3ff1de718febc5601e3

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
 # Add your code here
+
+class Dog
+#write a dog class
+#initializes with a name
+#stores each new instance of Dog that is instantiated
+attr_accessor :name, :save
+@@all = []
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    #write a class method #all that reads @@all
+
+    def self.all
+        @@all
+    end
+
+    #write a class method #print_all that interates over the individual dogs
+    #puts name to the terminal
+    def self.print_all
+       puts @@all.collect {|dog| dog.name}
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+   
+    def save
+        @@all << self
+    end
+end
